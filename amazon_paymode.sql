9c566d01bd2187e2ea2d8801494b03bc9e98995f
Vijay Kumar <71079648+kumarvijay82104@users.noreply.github.com>
2024-01-27 13:46:40 +0530

    Create amazon_paymode.sql

---
+++ amazon_paymode.sql
@@ -0,0 +1,26 @@
+create table SALE (merchant varchar(30), amount int, pay_mode varchar(10));
+
+insert into SALE values('merchant_1',150,'cash');
+insert into SALE values('merchant_1',500,'online');
+insert into SALE values('merchant_2',450,'online');
+insert into SALE values('merchant_1',100,'cash');
+insert into SALE values('merchant_3',600,'cash');
+insert into SALE values('merchant_5',200,'online');
+insert into SALE values('merchant_2',100,'online');
+
+---------First Method
+
+SELECT merchant, COALESCE(SUM(amount) FILTER (WHERE pay_mode = 'cash'), 0) AS mode_cash,
+COALESCE(SUM(amount) FILTER (WHERE pay_mode = 'online'), 0) AS mode_online
+FROM sale
+GROUP BY 1
+ORDER BY 1;
+
+---------Second Method
+
+with cte as (
+select merchant,(case when pay_mode ='cash' then amount else 0 end) cash_mode,
+(case when pay_mode ='online' then amount else 0 end) online_mode from sale
+group by 1,2,3)
+select merchant,sum(cash_mode),sum(online_mode) from cte
+group by 1 order by 1
