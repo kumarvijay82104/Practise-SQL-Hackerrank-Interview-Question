df9b3bf9e90196a91894af42fdeab897bc38895f
Vijay Kumar <71079648+kumarvijay82104@users.noreply.github.com>
2024-02-20 14:33:58 +0530

    Update leetcode.sql

--- leetcode.sql
+++ leetcode.sql
@@ -11,3 +11,13 @@ with cte as
         (select *,lead(num,1) over( )as next1,lead(num,2)over() as next2 from logs )
 select distinct num as ConsecutiveNums from cte
 where num = next1 and num = next2 ;
+
+'''Write a solution to find employees who have the highest salary in each of the departments.'''
+with cte as (
+            select e.id,e.name ,e.salary,d.name as Department from employee e join department d 
+                    on e.departmentid = d.id ),    
+    cte_2 as (
+             select *,rank() over(partition by Department order by salary desc ) as drsal from cte )
+select Department,name as Employee,Salary from cte_2
+where drsal = 1
+order by Employee
