6b18342f8fc180bca860f7da67b8674e4a54eb2c
Vijay Kumar <71079648+kumarvijay82104@users.noreply.github.com>
2024-02-20 12:18:03 +0530

    Update leetcode.sql

--- leetcode.sql
+++ leetcode.sql
@@ -4,4 +4,10 @@ with cte as (
                row_number() over(order by salary desc) as rn from
                                  (select distinct salary from Employee))
 select  rnsal as SecondHighestSalary from cte 
-where rn = 1
+where rn = 1;
+
+'''Find all numbers that appear at least three times consecutively.'''
+with cte as 
+        (select *,lead(num,1) over( )as next1,lead(num,2)over() as next2 from logs )
+select distinct num as ConsecutiveNums from cte
+where num = next1 and num = next2 ;
