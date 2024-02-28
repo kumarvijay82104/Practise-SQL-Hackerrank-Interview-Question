152683f52371cbae94b3d86f8124f07c4190cc19
Vijay Kumar <71079648+kumarvijay82104@users.noreply.github.com>
2024-02-28 13:38:54 +0530

    Update leetcode.sql

--- leetcode.sql
+++ leetcode.sql
@@ -50,6 +50,16 @@ with cte as(
             group by e.id ,e.name )
 select name from cte_2 ;
 
+'''Write a solution to find the people who have the most friends and the most friends number.
+The test cases are generated so that only one person has the most friends.'''
+with cte as (
+        select requester_id as id from RequestAccepted
+        union all
+        select accepter_id as id from RequestAccepted)
+select id, count(*) as num from cte
+group by id 
+order by count(*) desc limit 1
+
 
 
 
